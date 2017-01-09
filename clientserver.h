e965e3d5c55b29ef95f9df1fd6ff7db02a5e61ef
Shani Levy <shanil@jfrog.com>
2017-01-09 17:10:46 +0200

    Add files via upload

---
+++ clientserver.h
@@ -0,0 +1,16 @@
+/*********************************************
+Author : Eran Blumenthal
+Creation Date : 02-01-2014
+Modification Date : 06-01-2014
+Description : 
+**********************************************/
+
+#ifndef __CLIENT_SERVER_H__
+#define __CLIENT_SERVER_H__
+
+#define	IP_ADDRESS "127.0.0.1"
+#define	SERVER_PORT 5060
+
+
+#endif /* __CLIENT_SERVER_H__ */
+
